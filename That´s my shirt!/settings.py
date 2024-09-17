42bfb0789298bde0a83425fd26cdf3984bb3783c
Adrian <adriangomis34@gmail.com>
2024-09-17 11:23:10 +0200

    Update settings.py

--- That´s my shirt!/settings.py
+++ That´s my shirt!/settings.py
@@ -79,3 +79,7 @@ STORAGES = {
     "default": {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
     },
+ "staticfiles": {
+        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+    },
+}
