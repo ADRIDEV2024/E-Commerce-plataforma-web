9460caeee8985c08765283ea0b9c675346560cac
Adrian <adriangomis34@gmail.com>
2024-09-20 09:07:34 +0200

    Update settings.py

--- That´s my shirt!/settings.py
+++ That´s my shirt!/settings.py
@@ -8,7 +8,7 @@ from pathlib import Path
 load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent
 
 import json 
 
