3dfd32f35993b27b3d63cb82cc6c1208f1c89680
Adrian <adriangomis34@gmail.com>
2024-09-23 12:25:53 +0200

    Update settings.py

--- That´s my shirt!/settings.py
+++ That´s my shirt!/settings.py
@@ -14,7 +14,7 @@ import json
 
 DATABASES = {
         'default': {
-            "ENGINE": 'django.db.backends.mysql',
+            "ENGINE": 'django.db.backends.postgresql',
             "NAME": os.getenv(MY_DB_NAME),
             "USER": os.getenv(USERNAME),
             "PASSWORD": os.getenv(PASSWORD_FOR_DB),
