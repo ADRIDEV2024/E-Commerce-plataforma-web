ab638c263ea99d7ae759c62e267699f5a2d4260b
Adrian <adriangomis34@gmail.com>
2024-09-30 09:30:12 +0200

    Update settings.py

--- That´s my shirt!/settings.py
+++ That´s my shirt!/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = "That´s my shirt!.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [os.path.join(BASE_DIR, "templates"),],
+        "DIRS": [os.path.join(BASE_DIR, "templates")],
         "APP_DIRS": True,
         "OPTIONS":{
             "context_processors": [
@@ -65,10 +65,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
-            ],
-        },
-    },
-]
+            ],},},]
 
 
 ASGI_APPLICATION = "That´s my shirt!.asgi.application"
