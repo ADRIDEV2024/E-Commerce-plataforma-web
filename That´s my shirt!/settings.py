4dee7ede6347eef5941063d49d306e582d09ee98
Adrian <adriangomis34@gmail.com>
2024-09-30 09:27:37 +0200

    Update settings.py

--- That´s my shirt!/settings.py
+++ That´s my shirt!/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv(SECRET_KEY)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = [LOCAL_HOST]
 
 
 # Application definition
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     'store',
+    "models",
 ]
 
 
