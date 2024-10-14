edf3136a7342d8fd83cfbcf9d722265b04b9aec3
Adrian <adriangomis34@gmail.com>
2024-10-14 10:38:58 +0200

    Update urls.py

--- store/urls.py
+++ store/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.conf.urls.static import static 
 from django.urls import path,include
 
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("",include("store.urls")),
